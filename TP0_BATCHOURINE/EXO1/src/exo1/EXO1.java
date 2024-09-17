e04ea206830bc40f5560cc5a435ec5d8917af62a
sachabatchourine <sachabatchourine@localhost>
2024-09-17 17:02:05 +0200

    Exo1

---
+++ TP0_BATCHOURINE/EXO1/src/exo1/EXO1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ * Batchourine sacha
+ * TDC
+ * @author sachabatchourine
+ */
+public class EXO1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+        System.out.println("au revoir");
+        String prenom;
+Scanner sc;
+sc = new Scanner(System.in); System.out.println("Bonjour, quel est votre Nom ?"); prenom = sc.nextLine();
+    }
+    
+}
